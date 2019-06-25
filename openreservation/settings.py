bdbfd2c742da178e4b9a65fcde078d33e1bf163f
Sergio Ocon <sergio.ocon@redhat.com>
2019-06-25 10:13:55 -0400

    whitenoise middleware

--- openreservation/settings.py
+++ openreservation/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'openreservation.urls'
@@ -142,3 +143,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
