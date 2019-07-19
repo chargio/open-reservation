8476c37d81d4f2fa461237b468fb79c0bbd5479e
Sergio Ocon <sergio.ocon@redhat.com>
2019-07-19 09:12:04 +0200

    export to csv

--- offsprings/admin.py
+++ offsprings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Offspring
+import csv
+from django.http import HttpResponse
 
 
 # Register your models here.
@@ -8,6 +10,26 @@ class OffspringAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'grade',
                     'parent', 'assignment', 'baptized')
     list_display_links = ('id', 'first_name', 'last_name')
+
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
+            meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field)
+                                   for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Exportar seleccionados"
 
 
 admin.site.register(Offspring, OffspringAdmin)
