ca822bfc73a6c9b8f71e1a4a81154791b6162557
Sergio Ocón <sergio.ocon@gmail.com>
2019-08-04 14:12:05 +0200

    adding delete button and making sure that a user can only delete his profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     # You can only see your profile
     path('', views.ProfileDetailView.as_view(), name='profile'),
+    path('<int:pk>/delete', views.ProfileDeleteView.as_view(), name='delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,19 @@
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-
+from accounts.models import User
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     def get(self, request):
         return render(request, 'accounts/profile.html', {'user': self.request.user})
+
+
+class ProfileDeleteView(LoginRequiredMixin, DeleteView):
+    model = User
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return get_object_or_404(User, pk=self.request.user.id)
