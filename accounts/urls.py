76a7c5ed7ae38d29a06dae3759a5b916c358a3be
Sergio Ocon <sergio.ocon@redhat.com>
2019-08-14 10:05:58 +0200

    making user delete work and show a success screen

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # You can only see your profile
     path('', views.ProfileDetailView.as_view(), name='profile'),
     path('<int:pk>/delete', views.ProfileDeleteView.as_view(), name='delete'),
+    path('user_delete_success', views.ProfileDeleteSuccess.as_view(),
+         name="user_delete_success")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from django.shortcuts import render
 from accounts.models import User
 from django.urls import reverse_lazy
@@ -14,7 +15,12 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
 
 class ProfileDeleteView(LoginRequiredMixin, DeleteView):
     model = User
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('accounts:user_delete_success')
+    success_message = "%(full_name)s se ha borrado con éxito"
 
     def get_object(self):
         return get_object_or_404(User, pk=self.request.user.id)
+
+
+class ProfileDeleteSuccess(TemplateView):
+    template_name = "accounts/user_delete_success.html"
